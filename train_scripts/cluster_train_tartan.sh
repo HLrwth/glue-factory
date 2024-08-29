a6e6b58d513f06654109c197fb707a7ae5224f68
hli <hli@login2.is.localnet>
2024-08-29 19:18:52 +0200

    train tartan and continue train lg

---
+++ train_scripts/cluster_train_tartan.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# prepare dataset
+#mksquashfs ./datasets ./datasets.squashfs -noI -noD -noF -noX -no-fragments -no-duplicates
+
+mkdir /tmp/.cache
+export PYTORCH_KERNEL_CACHE_PATH=/tmp/.cache
+
+mkdir -p /tmp/gluefactory_train/data/datasets
+squashfuse /is/cluster/fast/hli/tartan/datasets.squashfs /tmp/gluefactory_train/data/datasets
+cp -r /home/hli/glue-factory/gluefactory /tmp/gluefactory_train/
+cd /tmp/gluefactory_train
+
+source /home/hli/glue-factory/train_scripts/setCUDAenv.sh 11.8
+/is/cluster/hli/miniconda3/envs/attnvo/bin/python -m gluefactory.train "$1" --conf gluefactory/configs/superpoint+simpleglue_tartanair.yaml
+
+TARGET_DIR="/is/cluster/fast/hli/glue"
+mkdir -p "$TARGET_DIR"
+cp -r /tmp/gluefactory_train/outputs "$TARGET_DIR"
