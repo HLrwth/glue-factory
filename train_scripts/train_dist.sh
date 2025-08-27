b6973f9271c67a30a1dd8682a1796f3b6d4d0af4
Tim (Slurm R1) <tim.z.xiao@outlook.com>
2025-08-27 23:30:16 +0200

    homography_geometry_double_head

--- train_scripts/train_dist.sh
+++ train_scripts/train_dist.sh
@@ -25,6 +25,6 @@ git add -u
 git commit -m "homography_geometry_double_head"
 
 source ~/.bashrc
-conda activate $WORK/.miniconda3/envs/haolong_glue
+conda activate $WORK/.miniconda3/envs/haolong
 srun python -m gluefactory.train homography_geometry_double_head --conf gluefactory/configs/superpoint+simpleglue_homography.yaml  \
             --mp bfloat16 --distributed model.matcher.checkpointed=True
